701bade6101431b5b2480d0af9e422304dbb27c4
Rajiv Chaulagain <82865638+rajivchaulagain@users.noreply.github.com>
2023-04-15 15:38:41 +0545

    rocks scissor paper

---
+++ jsPractise/challengeOne.js
@@ -0,0 +1,10 @@
+function challengeOne() {
+    let tshirt = 10;
+    let socks = 8;
+    let dinnerPlate = 20;
+    
+    let total = tshirt + socks + dinnerPlate;
+    return total;
+}
+
+export default challengeOne
